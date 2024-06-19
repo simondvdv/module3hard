ddbbfbe65eb55dc5ef0e3841968c49e614612b15
simondvdv <simondv4@gmail.com>
2024-06-19 14:03:37 +0300

    second commit

--- module3hard.py
+++ module3hard.py
@@ -5,12 +5,16 @@ def sum_collection(collection):
         match type_check(i):
             case 'list':
                 summator += sum_list(i)
+                print('list')
             case 'dict':
                 summator += sum_dict(i)
+                print('dict')
             case 'str':
                 summator += len(i)
+                print('str')
             case 'int':
                 summator += i
+                print('int')
     return summator
 
 
@@ -32,6 +36,8 @@ def sum_list(list_):
             summator += i
         elif isinstance(i, str):
             summator += len(i)
+        elif isinstance(i, dict):
+            summator += sum_dict(i)
         else:
             return summator + sum_list(i)
     return summator
@@ -40,14 +46,17 @@ def sum_list(list_):
 def sum_dict(dict_):
     summator = 0
     for key, value in dict_.items():
-        summator += sum_collection(key)
-        summator += sum_collection(value)
+        print(key)
+        print(value)
+        summator += len(key)
+        summator += value
     return summator
 
 
 data_structure = [[1, 2, 3], {'a': 4, 'b': 5}, (6, {'cube': 7, 'drum': 8}), "Hello",
                   ((), [{(2, 'Urban', ('Urban2', 35))}])]
+answer = sum_collection([[1, 2, 3], {'a': 4, 'b': 5}])
 answer = sum_collection(data_structure)
 print(answer)
 
-
+print(type(()))
